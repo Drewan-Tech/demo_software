c686fdea81141fb37ee14e024475491c5197217c
Benton Drew <benton.s.drew@drewantech.com>
2016-04-25 14:47:41 -0700

    Move is number check to common module and implemented new module.

--- generate_matrix_pairs/create_random_matrix.py
+++ generate_matrix_pairs/create_random_matrix.py
@@ -2,8 +2,6 @@
 #  Drewan Tech, LLC
 #  ALL RIGHTS RESERVED
 
-import random
-
 
 def create_matrix(number_of_rows,
                   number_of_columns,
@@ -11,25 +9,20 @@ def create_matrix(number_of_rows,
   """Will create a matrix with the given size. The random numbers generated
   will be in the range of 0 to the scaling factor.
   """
+  import random
+  from drewantech_common.value_checks import is_number_type_not_complex
   if type(number_of_rows) is not int:
     raise ValueError('Number of rows arg passed to create_matrix function is '
                      'not an int type')
   if type(number_of_columns) is not int:
     raise ValueError('Number of columns arg passed to create_matrix function '
                      'is not an int type')
-  if not valid_scaling_factor_type(scaling_factor):
+  if not is_number_type_not_complex(scaling_factor):
     raise ValueError('Scaling factor arg passed to create_matrix function '
                      'is not an int or float type.')
   return [[(random.random() * scaling_factor)
            for column in range(number_of_columns)]
           for row in range(number_of_rows)]
-
-
-def valid_scaling_factor_type(scaling_factor):
-  if type(scaling_factor) is not int:
-    if type(scaling_factor) is not float:
-      return False
-  return True
 
 
 if __name__ == '__main__':
