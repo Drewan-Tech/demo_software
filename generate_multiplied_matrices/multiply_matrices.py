34c01b8bce25320fd4e5c61bacd7dafb209d0152
Benton Drew <benton.s.drew@drewantech.com>
2016-05-04 12:05:20 -0700

    Have multiply matrices module create instance id and change generated file name to contain instance id, module name and matrix name.

--- generate_multiplied_matrices/multiply_matrices.py
+++ generate_multiplied_matrices/multiply_matrices.py
@@ -4,27 +4,37 @@
 
 
 if __name__ == '__main__':
-  import argparse
-  from drewantech_common.value_checks import valid_directory
-  from drewantech_common.string_generator \
-      import generate_32_character_random_string
-  import json
-  from matrix_functions import multiply as multiply_matrices
-  parser = argparse.ArgumentParser(description='Multiplies two matrices.')
-  parser.add_argument('matrix_a_file',
-                      type=argparse.FileType(mode='r'),
-                      help='File containing matrix A data.')
-  parser.add_argument('matrix_b_file',
-                      type=argparse.FileType(mode='r'),
-                      help='File containing matrix B data.')
-  parser.add_argument('output_directory',
-                      type=valid_directory,
-                      help='Directory to write file containing product.')
-  args = parser.parse_args()
-  multiplied_matrix = (multiply_matrices(json.load(args.matrix_a_file),
-                                         json.load(args.matrix_b_file)))
-  file_location_and_name = ('{}/{}.asc'
-                            .format(args.output_directory,
-                                    generate_32_character_random_string()))
-  with open(file_location_and_name, 'w') as matrix_write:
-    json.dump(multiplied_matrix, matrix_write)
+  import sys
+  try:
+    import argparse
+    from drewantech_common.value_checks import valid_directory
+    from drewantech_common.string_generator \
+        import generate_32_character_random_string
+    import json
+    from matrix_functions import multiply as multiply_matrices
+    import os
+    parser = argparse.ArgumentParser(description='Multiplies two matrices.')
+    parser.add_argument('matrix_a_file',
+                        type=argparse.FileType(mode='r'),
+                        help='File containing matrix A data.')
+    parser.add_argument('matrix_b_file',
+                        type=argparse.FileType(mode='r'),
+                        help='File containing matrix B data.')
+    parser.add_argument('output_directory',
+                        type=valid_directory,
+                        help='Directory to write file containing product.')
+    args = parser.parse_args()
+    instance_id = generate_32_character_random_string()
+    module_name = (os.path.basename(__file__).rstrip('.py'))
+    multiplied_matrix = (multiply_matrices(json.load(args.matrix_a_file),
+                                           json.load(args.matrix_b_file)))
+    file_location_and_name = ('{}/{}_{}_Matrix_A.asc'
+                              .format(args.output_directory,
+                                      instance_id,
+                                      module_name))
+    with open(file_location_and_name, 'w') as matrix_write:
+      json.dump(multiplied_matrix, matrix_write)
+    sys.exit(0)
+  except Exception as e:
+    print(e)
+    sys.exit(1)
