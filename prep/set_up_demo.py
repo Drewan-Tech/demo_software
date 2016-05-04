aecf32827c16040fe164203763f88df464e5b4d2
Benton Drew <benton.s.drew@drewantech.com>
2016-05-04 11:09:24 -0700

    Change name of infrastructure module to demo_infrastructure.

--- prep/set_up_demo.py
+++ prep/set_up_demo.py
@@ -3,6 +3,6 @@
 #  ALL RIGHTS RESERVED
 
 if __name__ == '__main__':
-  from infrastructure import demo_database
+  from demo_infrastructure import demo_database
   demo_database.create_database()
   demo_database.create_tables()
--- prep/tear_down_demo.py
+++ prep/tear_down_demo.py
@@ -4,6 +4,6 @@
 
 
 if __name__ == '__main__':
-  from infrastructure import demo_database
+  from demo_infrastructure import demo_database
   demo_database.drop_tables()
   demo_database.drop_database()
