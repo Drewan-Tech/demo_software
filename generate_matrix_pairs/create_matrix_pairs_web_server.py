c435404c1ab6b46a5f0293b3d471a90527dd6a65
Benton Drew <benton.s.drew@drewantech.com>
2016-06-08 18:32:01 -0700

    Implement basic web server for creating matrix pairs.

--- generate_matrix_pairs/create_matrix_pairs_web_server.py
+++ generate_matrix_pairs/create_matrix_pairs_web_server.py
@@ -3,12 +3,21 @@
 #  ALL RIGHTS RESERVED
 
 
-from flask import Flask
+from flask import (Flask,
+                   render_template,
+                   request)
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return ('Create Matrix Pairs')
+  if request.method == 'GET':
+    return render_template('index_get.html')
+  else:
+    if request.method == 'POST':
+      return render_template('index_post.html',
+                             A_rows_B_columns=request.form['A_rows_B_columns'],
+                             B_rows_A_columns=request.form['B_rows_A_columns'],
+                             max_random_value=request.form['max_random_value'])
