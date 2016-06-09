7992a3816f1cfd51d25a2a91709d9faf514f086c
Benton Drew <benton.s.drew@drewantech.com>
2016-06-08 20:52:28 -0700

    Implementing URL for generating the matrix pairs. Temporary hard-code output location.

--- generate_matrix_pairs/create_matrix_pairs_web_server.py
+++ generate_matrix_pairs/create_matrix_pairs_web_server.py
@@ -6,7 +6,9 @@
 from flask import (Flask,
                    render_template,
                    request,
-                   flash)
+                   flash,
+                   url_for,
+                   redirect)
 from os import urandom
 
 
@@ -17,7 +19,7 @@ app.secret_key = urandom(32)
 @app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'GET':
-    return render_template('index_get.html')
+    return render_template('index.html')
   else:
     if request.method == 'POST':
       try:
@@ -26,26 +28,33 @@ def index():
       except:
         flash('Please provide integer values for the number of rows and '
               'columns.')
-        return render_template('index_get.html')
+        return render_template('index.html')
       try:
         float(request.form['max_random_value'])
       except:
         flash('Please provide a valid number for the maximum random number.')
-        return render_template('index_get.html')
-      try:
-        from os import path
-        if not path.isdir(request.form['output_location']):
-          raise OSError('Path not valid.')
-      except:
-        flash('{} is not a valid path.'
-              .format(request.form['output_location']))
-        return render_template('index_get.html')
-      return render_template('index_post.html',
-                             A_rows_B_columns=request
-                             .form['A_rows_B_columns'],
-                             B_rows_A_columns=request
-                             .form['B_rows_A_columns'],
-                             max_random_value=request
-                             .form['max_random_value'],
-                             output_location=request
-                             .form['output_location'])
+        return render_template('index.html')
+      return redirect(url_for('matrix_pair',
+                      A_rows_B_columns=int(request
+                                           .form['A_rows_B_columns']),
+                      B_rows_A_columns=int(request
+                                           .form['B_rows_A_columns']),
+                      max_random_value=float(request
+                                             .form['max_random_value'])))
+
+
+@app.route('/matrix_pair'
+           '/<int:A_rows_B_columns>'
+           '/<int:B_rows_A_columns>'
+           '/<float:max_random_value>'
+           '/')
+def matrix_pair(A_rows_B_columns,
+                B_rows_A_columns,
+                max_random_value):
+  flash('Generating matrix pair.')
+  return render_template('matrix_pair.html',
+                         A_rows_B_columns=A_rows_B_columns,
+                         B_rows_A_columns=B_rows_A_columns,
+                         max_random_value=max_random_value,
+                         output_location=('/home/benton/drewan_tech'
+                                          '/demo_output/'))
